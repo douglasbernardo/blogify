ea7b93ff6d2b17197ad7eb6740b48bb384e151a0
Douglas Melo <douglas@emiolo.com>
2023-12-30 11:19:01 -0300

    fixing comment in first when user is logged, improving layout

--- pwa/pages/artigos/reading/[id]/index.vue
+++ pwa/pages/artigos/reading/[id]/index.vue
@@ -17,7 +17,7 @@
       </article>
       <v-card-actions class="d-flex justify-end">
         <v-btn class="ma-2" size="small" variant="flat" color="primary" rounded @click="$router.push('/')">Voltar</v-btn>
-        <v-btn v-if="authStore.token" class="ma-2" variant="flat" size="small" color="primary" rounded append-icon="mdi-comment" @click="dialogCreateComment=!dialogCreateComment">Comentar</v-btn>
+        <v-btn class="ma-2" variant="flat" size="small" color="primary" rounded append-icon="mdi-comment" @click="sheet=!sheet">Comentarios</v-btn>
         <v-btn class="ma-2" size="small" variant="flat" append-icon="mdi-share" rounded color="primary">Compartilhar</v-btn>
       </v-card-actions>
     </v-card>
@@ -25,36 +25,88 @@
       <v-card>
         <div class="ma-2 pa-2">
           <v-text-field label="Nome" v-model="my_name" disabled></v-text-field>
-          <v-text-field label="Comentário"></v-text-field> 
+          <v-text-field label="Comentário" v-model="my_comment_text"></v-text-field> 
         </div>
         <v-card-actions>
           <v-btn color="primary" size="small" @click="dialogCreateComment = false" variant="flat" rounded>Cancelar</v-btn>
-          <v-btn color="primary" size="small" @click="comment(articleOptions._id)" variant="flat" rounded>Comentar</v-btn>
+          <v-btn color="primary" size="small" @click="funcComment(articleOptions._id)" variant="flat" rounded>Comentar</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <v-bottom-sheet v-model="sheet" inset>
+      <v-card
+        class="text-center"
+        height="2000"
+      >
+      <v-list lines="two">
+          <v-btn v-if="authStore.token" size="small" class="d-flex justify-start ma-2" variant="tonal" color="primary" @click="dialogCreateComment=!dialogCreateComment">Criar Comentario</v-btn>
+          <v-list-item
+            class="text-left"
+            v-for="comment in fixedComment"
+            :key="comment._id"
+            :prepend-avatar="comment.authorImage"
+          >
+            <v-list-item-content>
+              <v-chip 
+                v-if="comment.authorEmail === my_email"
+                class="text-left ma-2" 
+                prepend-icon="mdi-account" 
+                color="primary" 
+                variant="outlined"
+              >
+                Meu Comentário
+              </v-chip>
+              <v-list-item-title>{{ comment.authorName }}</v-list-item-title>
+              <v-list-item-subtitle>{{ comment.text }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-bottom-sheet>
   </v-container>
 </template>
 
 <script lang="ts" setup>
-  import axios from "axios";
-import { useAuthStore } from "~/store/user/authStore";
+  import { useAuthStore } from "~/store/user/authStore";
+  import { useCommentStore } from "~/store/comment_manager";
   const route = useRoute()
-  const articleOptions = ref<string>(null)
+  const articleOptions = ref<string | null>(null)
   const authStore = useAuthStore()
-  const dialogCreateComment = ref(false)
+  const dialogCreateComment = ref<boolean>(false)
   const my_name = ref<string>(String(localStorage.getItem('name')))
+  const my_email = ref<string>(String(localStorage.getItem('user')))
+  const sheet = ref(false)
+  const comment = useCommentStore()
+  const my_comment_text = ref<string>('')
+  const commentsArray = ref<Array<string>>([])
+
   onMounted(async ()=>{
-    const resp = await api_call({method:'get',url:`/article/reading/${route.params.id}`})
-    resp ? articleOptions.value = JSON.parse(resp) : articleOptions.value
+    const [respArticle, respComments] = await Promise.all([
+      api_call({method:'get',url:`/article/reading/${route.params.id}`}),
+      api_call({
+        method:'post',
+        url:`/comment/all`, 
+        data: { id: route.params.id }
+      })
+    ])
+    respArticle ? articleOptions.value = JSON.parse(respArticle) : []
+    respComments ? commentsArray.value = JSON.parse(respComments) : []
   })
 
-  const comment = async(idArticle: string) => {
-    await axios.post('http://localhost:3030/comment', {
+  const funcComment = async(idArticle: string) => {
+    await comment.create({
       author: localStorage.getItem('name'),
       emailAuthor: localStorage.getItem('user'),
-      text: 'hello heres a text just a test',
+      text: my_comment_text.value,
       idArticle: String(idArticle),
     })
   }
+
+  const fixedComment = computed(() => {
+    //when user is logged his comment gets fixed in first if he comments something in ther article
+    return commentsArray.value.slice().sort((my_comment) => {
+      if (my_comment.authorEmail === my_email.value) return -1; // Se o autor for você, coloque primeiro
+      return 0; // Caso contrário, mantenha a ordem original
+    });
+  })
 </script>
---
+++ pwa/store/comment_manager.ts
@@ -0,0 +1,24 @@
+import {defineStore} from "pinia";
+import { api_call } from "../utils/api_call";
+
+export const useCommentStore = defineStore('comment',{
+  state: () => ({
+    comments: []
+  }),
+  actions: {
+    async create(commentData: object){
+      const new_comment = await api_call({
+        method: 'post',
+        url: '/comment',
+        data: commentData,
+      })
+    },
+
+    async get_comments(id: string){
+      await api_call({
+        method: 'get',
+        url: `/comment/all/${id}`,
+      }) 
+    }
+  }
+})
