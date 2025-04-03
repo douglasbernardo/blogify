8424fbcce08346bf84e4b83b6d5f7bd630d1dc49
Douglas Bernardo <douglasbernardomelo@gmail.com>
2025-04-03 13:51:39 -0300

    improving layout when users sees their own articles

--- pwa/components/FetchMyArticlesMobile.vue
+++ pwa/components/FetchMyArticlesMobile.vue
@@ -1,35 +1,45 @@
 <template>
-    <v-expansion-panels v-for="(article,index) in articles" :key="index">
+    <v-expansion-panels v-for="(article,index) in articles" :key="index" variant="popout">
         <v-expansion-panel :title="article.title">
             <v-expansion-panel-text>
-                <v-row class="ma-2">
-                    <v-list>
-                        <v-list-item title="Categoria">
-                            <template v-slot:subtitle>
-                                <span><v-chip density="comfortable">{{ article.category }}</v-chip></span>
-                            </template>
-                        </v-list-item>
-                        <v-list-item title="Likes">
-                            <template v-slot:subtitle>
-                                <span class="">Seu artigo tem <v-chip density="comfortable">{{ article.likes }}</v-chip> curtidas</span>
-                            </template>
-                        </v-list-item>
-                        <v-list-item title="Comentários">
-                            <template v-slot:subtitle>
-                                <span class="">Seu artigo tem <v-chip density="comfortable">{{ article.comments }}</v-chip> comentários</span>
-                            </template>
-                        </v-list-item>
-                        <v-list-item title="Vizualizações">
-                            <template v-slot:subtitle>
-                                <span class="">Seu tem <v-chip density="comfortable">{{ article.views }}</v-chip> vizualizações</span>
-                            </template>
-                        </v-list-item>
-                        <v-list-item title="Status">
-                            <template v-slot:subtitle>
-                                <span class="">Está com o status <v-chip density="comfortable" :color="article.status === 'publico' ? 'green' : 'red'">{{ article.status }}</v-chip></span>
-                            </template>
-                        </v-list-item>
-                    </v-list>
+                <v-list density="compact">
+                    <v-list-item>
+                    <template v-slot:prepend>
+                        <v-icon color="primary">mdi-tag</v-icon>
+                    </template>
+                    <v-list-item-title>Categoria: {{ article.category }}</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item>
+                    <template v-slot:prepend>
+                        <v-icon color="red">mdi-heart</v-icon>
+                    </template>
+                    <v-list-item-title>Curtidas: {{ article.likes }}</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item>
+                    <template v-slot:prepend>
+                        <v-icon color="blue">mdi-comment</v-icon>
+                    </template>
+                    <v-list-item-title>Comentários: {{ article.comments }}</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item>
+                    <template v-slot:prepend>
+                        <v-icon color="green">mdi-eye</v-icon>
+                    </template>
+                    <v-list-item-title>Visualizações: {{ article.views }}</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item>
+                        <template v-slot:prepend>
+                            <v-icon color="orange">mdi-information</v-icon>
+                        </template>
+                        <v-list-item-title>
+                            Status: <span :style="{color: article.status === 'publico' ? 'green' : 'red' }">{{ article.status }}</span>
+                        </v-list-item-title>
+                    </v-list-item>
+                </v-list>
                     <v-btn
                         class="ma-2 ml-6"
                         color="primary"
@@ -49,16 +59,15 @@
                             </v-list-item-title>
                             </v-list-item>
                         </v-list>
-                        </v-menu>
-                    </v-btn>
-                </v-row>
+                    </v-menu>
+                </v-btn>
             </v-expansion-panel-text>
         </v-expansion-panel>
     </v-expansion-panels>
 </template>
 
 <script setup lang="ts"> 
-defineProps({
+const props = defineProps({
     articles: Array
 })
 </script>
